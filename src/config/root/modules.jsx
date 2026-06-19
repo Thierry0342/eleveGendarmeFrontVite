3b7c124979cde248d3687366fbf2a6d83560c962
Thierry0342 <thrcasimir@gmail.com>
2026-06-19 16:18:32 +0200

    chore: ignore src/config/root

--- src/config/root/modules.jsx
+++
@@ -1,8 +0,0 @@
-// src/env/env.js
-
-const DEFAULT_IP = "https://hardhead-marbles-glass.ngrok-free.dev"; // <-- URL ngrok
-const savedIP = localStorage.getItem("CUSTOM_IP");
-export const ENV_MODE = "prod";
-export const IP = DEFAULT_IP;
-export const IMAGE_PATH = IP + "/data/uploads";
-export const API_URL = "https://hardhead-marbles-glass.ngrok-free.dev";
